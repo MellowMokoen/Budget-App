b651504ef3acbc4e20832f2992ed2b0dd99083b0
MellowMokoen <mamello3632@gmail.com>
2023-06-25 00:23:36 +0200

    Added few variables and attributes to the js file to display the results of the user input in a form of a list

--- src/script.js
+++ src/script.js
@@ -1,17 +1,28 @@
 function displayAmount(event) {
-    event.preventDefault();
+   event.preventDefault();
+
   let inputName = document.querySelector("#inputName");
   let inputAmount = document.querySelector("#amount");
   let amount = parseFloat(inputAmount.value).toFixed(2);
 
-  let display = document.querySelector("#display-amount");
-  
+  let userInput = (inputName.value) + " : " + "R " + amount;
 
-  display.innerHTML = (inputName.value) + " : " + "R " + amount;
+
+  let displayElement = document.createElement("li");
+  displayElement.textContent = userInput;
+
+  let displayResults = document.querySelector("#display-results");
+  displayResults.appendChild(displayElement);
+
+  document.querySelector("#inputName").value = "";
+  document.querySelector("#amount").value = "";
+
 }
 
 let button = document.querySelector("#log-button");
 button.addEventListener("click",displayAmount);
+
+
 
 
 
