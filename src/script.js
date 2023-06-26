92897da8883fc21a14f483a00d5155ab6a4f52b6
MellowMokoen <mamello3632@gmail.com>
2023-06-26 21:24:50 +0200

    Changed the background color and styled p2 using CSS

--- src/script.js
+++ src/script.js
@@ -11,8 +11,11 @@ function displayAmount(event) {
   let displayElement = document.createElement("li");
   displayElement.textContent = userInput;
 
-  let displayResults = document.querySelector("#display-results");
-  displayResults.appendChild(displayElement);
+  let displayName = document.querySelector("#display-name");
+  displayName.appendChild(displayElement);
+
+  let displayAmount = document.querySelector("#display-amount");
+  displayAmount.appendChild(displayElement);
 
   inputName.value = "";
   inputAmount.value = "";
@@ -25,17 +28,17 @@ button.addEventListener("click",displayAmount);
 function subtractAmount() {
 
 let inputSalary = document.querySelector("#underline-input");
-let inputAmount = document.querySelector("#amount");
+let inputAmount = document.querySelector("#display-amount");
 
 let userSalary = parseFloat(inputSalary.value);
-let amount = parseFloat(inputAmount.value);
+let amount = parseFloat(inputAmount.textContent);
 
 let finalAmount = userSalary - amount;
 
 let resultAmount = document.querySelector("#balance-check");
 resultAmount.textContent = "Expenses Total : R " + finalAmount.toFixed(2);
 
-inputSalary.value = "";
+
 inputAmount.value = "";
 }
 
